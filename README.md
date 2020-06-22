e73e0e3f20932f6145f28af48a036d2b6a2eb29e
Nicholas O'Kelley <leadslinger32@gmail.com>
2020-06-22 17:18:10 -0400

    Implemented user input for the file to read in. Then made it so a message is written during the program runtime in order for the final step to write the message to a file specified by a user

---
+++ README.md
@@ -0,0 +1,6 @@
+# Sudoku Solver
+
+This project is a small implementation of the backtracking algorithm
+to solve most sudoku boards.
+
+
--- soduko.py
+++ soduko.py
@@ -3,8 +3,6 @@
 # Author: Nicholas O'Kelley
 # Date: June 20, 2020
 
-
-
 def locate(board):
     #print("Locating valid entry...")
     # For each row, col 
@@ -81,12 +79,14 @@ def check_validity(board, num, position):
 
 
 def display_board(board):
+    message = ""
     # For the length of our array
     for i in range(len(board)):
         # we want to have this line after 3 rows, but
         # not at the start
         if i % 3 == 0 and i != 0:
             print("-------------------------")
+            message = message + "-------------------------\n"
 
         # Then for each column
         for j in range(len(board)):
@@ -94,27 +94,71 @@ def display_board(board):
             # breakup the 3 x 3 grids
             if j % 3 == 0 and j != 0:
                 print(" | ", end="")
+                message = message + " | "
             # If on the last iteration just the number
             # otherwise include the proper spacing
             if j == 8:
                 print(board[i][j])
+                message = message + str(board[i][j]) + "\n"
             else:
                 print(str(board[i][j]) + " ", end="")
+                message = message + str(board[i][j]) + " "
+    return message
+
+
+def process_output(board, isBoardSolved):
+
+    write_to_file = ""
+    if isBoardSolved:
+        print("\nSolution found!\n")
+        write_to_file = write_to_file + "\nSolution found!\n"
+    else:
+        print("\nNo solution found!")
+        write_to_file = write_to_file + "\nNo Solution Found\n"
+    write_to_file = write_to_file + display_board(board)
+    print("\n\n")
+    write_to_file = write_to_file + "\n\n"
+
+    return write_to_file
+
+def read_file(file):
+    try:
+        write_to_file = ""
+        board = [[0 for x in range(9)] for y in range(9)]
+
+        with open(file) as input_board:
+            write_to_file = write_to_file + input_board.readline()
+            i = 0
+            j = 0
+            for line in input_board:
+                line.rstrip()
+                j = 0
+                for c in line:
+                    board[i][j] = int(c)
+                    j = j + 1
+                    if j == 9:
+                        i = i + 1
+                        break
+                if i == 9:
+                    break
+        return write_to_file,board
+    except FileNotFoundError:
+        raise FileNotFoundError("ERROR: Could not find that board. Try again.")
+
+def write_file(message):
+    output = "./solved_boards/" + input("Enter output file name: ").strip() + ".txt"
+    with open(output, "w+") as f:
+        for line in message:
+            f.write(line)
+
 
 def main():
-    # this sample board was pulled from 
-    # Arizona Daily Wildcat: Tuesday, Jan 17th 2006
-    sample_board = [
-            [0, 0, 0, 2, 6, 0, 7, 0, 1],
-            [6, 8, 0, 0, 7, 0, 0, 9, 0],
-            [1, 9, 0, 0, 0, 4, 5, 0, 0],
-            [8, 2, 0, 1, 0, 0, 0, 4, 0],
-            [0, 0, 4, 6, 0, 2, 9, 0, 0],
-            [0, 5, 0, 0, 0, 3, 0, 2, 8],
-            [0, 0, 9, 3, 0, 0, 0, 7, 4],
-            [0, 4, 0, 0, 5, 0, 0, 3, 6],
-            [7, 0, 3, 0, 1, 8, 0, 0, 0],
-    ]
+    text_file = ("./unsolved_boards/" + input("Enter file: "))
+    message, board = read_file(text_file)
+    message = message + display_board(board)
+    result = solved_board(board)
+    message = message + process_output(board, result)
+    write_file(message)
 
     # This sample board was pulled from 
     # the book: The Algorithm Design Manual
@@ -129,25 +173,6 @@ def main():
             [0, 8, 0, 0, 0, 0, 0, 4, 0],
             [0, 5, 0, 0, 0, 0, 6, 0, 0]
     ]
-
-    print("Arizona Daily Wildcat Board\n")
-    display_board(sample_board)
-    result = solved_board(sample_board)
-    if result:
-        print("\nSolution found!")
-    else:
-        print("\nNo solution found!")
-    display_board(sample_board)
-    print("\n\n")
-
-    print("Hardest Board from Design Manual")
-    display_board(sample_board1)
-    result = solved_board(sample_board1)
-    if result:
-        print("\nSolution found!")
-    else:
-        print("\nNo solution found")
-    display_board(sample_board1)
 
 
 if __name__ == '__main__':
